2f165dc0c2afe1b34334ae43e16fef418ef80db0
michael-rua <michaelrua1@gmail.com>
2020-10-19 17:33:53 +1300

    start rn app

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
-import { StatusBar } from 'expo-status-bar'
+// import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Image, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import ImageOfBalloon from './ImageOfBalloon'
+
 export default function App () {
-  const pic = {
-    url: 'https://images.pexels.com/photos/3102818/pexels-photo-3102818.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-  }
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Image source={pic} style= {{ width: 100, height: 300 }}/>
+    <>
+      <View style={styles.container}>
+        {/* <StatusBar style="auto" /> */}
+        <Text style={{ top: 200, textAlign: 'center', color: '#888', fontSize: 18, zIndex: 1 }}>
+        Chores Time!
+        </Text>
+      </View>
       <ImageOfBalloon/>
-    </View>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    backgroundImage: 'https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    zIndex: 1
   }
 })
--- ImageOfBalloon.jsx
+++ ImageOfBalloon.jsx
@@ -4,10 +4,10 @@ import { ImageBackground } from 'react-native'
 export default class Bananas extends Component {
   render () {
     let pic = {
-      url: 'https://images.pexels.com/photos/3102818/pexels-photo-3102818.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      url: 'https://images.pexels.com/photos/4239041/pexels-photo-4239041.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
     }
     return (
-      <ImageBackground source={pic} style={{ width: 450, height: 900 }}/>
+      <ImageBackground source={pic} style={{ width: 450, height: 920, zIndex: -1 }}/>
     )
   }
 }
